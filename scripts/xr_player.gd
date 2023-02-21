30b248125a9bed9a2f884ceeb7706808556f088c
Colin <smithcol@pdx.edu>
2023-02-21 12:12:17 -0800

    Using godot-xr-tools addon. Also, created prefabs from game scene objects to instantiate. Committing to save place, attempting to reimport game.glb with embedded textures.

--- scripts/xr_player.gd
+++ scripts/xr_player.gd
@@ -1,22 +1,16 @@
-extends CharacterBody3D
+extends Node3D
 
 @onready var camera: XRCamera3D = $XROrigin3D/XRCamera3D
 @onready var origin: XROrigin3D = $XROrigin3D
 @onready var left_hand: XRController3D = $XROrigin3D/LeftHand
 @onready var right_hand: XRController3D = $XROrigin3D/RightHand
+@onready var footsteps: AudioStreamPlayer3D = $XROrigin3D/PlayerBody/SoundFootsteps
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var move = $XROrigin3D/LeftHand.get_input("primary")
-	
-	if move:
-		var forward: Vector3 = camera.transform.basis * Vector3(move.y, 0, move.x)
-		velocity = Vector3(forward.x, 0, forward.z).normalized()
-	else: velocity = Vector3.ZERO
-	
-	$CollisionShape3D.position = Vector3($XROrigin3D/XRCamera3D.position.x, .5, $XROrigin3D/XRCamera3D.position.z)
-	
-	move_and_slide()
-	
+	if left_hand.is_button_pressed("primary") and !footsteps.playing:
+		footsteps.play()
+	elif !left_hand.is_button_pressed("primary") and footsteps.playing:
+		footsteps.stop()
